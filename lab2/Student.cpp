468634ca78a2f40f8d71babd8f324187c7e38776
OksanaSydor <sydor.oksana1111@gmail.com>
2026-03-05 23:08:04 +0200

    Correct Student constructor overloading again

--- lab2/Student.cpp
+++ lab2/Student.cpp
@@ -3,26 +3,13 @@
 
 using namespace std;
 
-Student::Student()
-{
-  cout << "Student created" << endl;
-}
-
 Student::Student(string name, int age, string faculty) : name(name), age(age), faculty(faculty)
 {
+  this->name = name;
   this->age = age;
   this->faculty = faculty;
 
   cout << "Student created via parameters" << endl;
-}
-
-Student::Student(string name)
-{
-  this->name = name;
-  this->age = 0;
-  this->faculty = "Unknown";
-
-  cout << "Student created via parameter \"name\"" << endl;
 }
 
 Student::~Student()
